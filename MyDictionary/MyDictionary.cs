d04b38dba97ba3385af122f7a00338556a365fa6
Furkan <furkanyilmaz-@hotmail.com>
2021-02-05 17:17:10 +0300

    MyDictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TModel, TAge>
+    {
+        TModel[] models;
+        TAge [] ages;
+
+        public MyDictionary()
+        {
+            models = new TModel[0];
+            ages = new TAge[0];
+        }
+
+        public void Add(TModel model, TAge age)
+        {
+            TModel [] tempModel = models;
+            models = new TModel[tempModel.Length+1];
+
+            for (int i = 0; i < tempModel.Length; i++)
+            {
+                models[i] = tempModel[i];
+            }
+
+            models[models.Length - 1] = model;
+
+            TAge[] tempAge = ages;
+            ages = new TAge[tempAge.Length + 1];
+
+            for (int i = 0; i < tempAge.Length; i++)
+            {
+                ages[i] = tempAge[i];
+            }
+
+            ages[ages.Length - 1] = age;
+
+            Console.WriteLine();
+
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < models.Length; i++)
+            {
+                Console.WriteLine("Model: " + models[i] + " yaş: "+ ages[i]) ;
+            }
+        }
+
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> names = new MyDictionary.MyDictionary<string, string>();
+
+            names.Add("Honda civic", "2008");
+            names.Add("citroen c5", "2012");
+            names.Add("Alfa Romeo", "2006");
+
+
+            names.List();
+            
+        }
+    }
+}
